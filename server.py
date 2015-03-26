92facfba886aea88b0f2c8113a8adea473d0d572
Praveen Alam <praveen.alam@gmail.com>
2015-03-25 22:29:47 -0400

    Server tests divided into modules

--- server.py
+++ server.py
@@ -8,53 +8,38 @@ import random
 
 class middleboxHttphandler(BaseHTTPServer.BaseHTTPRequestHandler):
     #@overide do_get
-	def do_GET(self):
-		HttpProxy = False
-		NatProxy = False
-		responseDict ={}
-		knownUserAgents =['TEST']
-		print(self.headers)
-		reqHeaders = self.headers
-		#if there was a middlebox HTTP standard suggest to include via header field
-		if 'Via' in reqHeaders.keys():
-			HttpProxy = True
+    def do_GET(self):
+        HttpProxy = False
+        NatProxy = False
+        responseDict ={}
+        testType = None
+        print(self.headers)
+        reqHeaders = self.headers
+        if "Test-Type" in reqHeaders.keys():
+            testType = reqHeaders['Test-Type']
+        #if there was a middlebox HTTP standard suggest to include via header field
+        if testType == "HeaderTest":
+            HttpProxy = checkHeaderManipulation(reqHeaders)
+            self.send_response(200)
+            self.end_headers()
+            if not HttpProxy:
+                #Headers were not manipulated
+
+                self.wfile.write("HTTP_HEADER_OK")
+            else:
+                #Headers were manipulated
+                self.wfile.write("HTTP_HEADER_MANIPULATED")
+
+
+    def checkHeaderManipulation(self,headers):
+        knownUserAgents =['TEST']
+        if 'Via' in reqHeaders.keys():
+			return True
 		#check the user agent string, if it is not TEST, then it is modified by http proxy
-		for uagent in knownUserAgents:
-			if uagent not in reqHeaders['User-Agent']:
-				HttpProxy = True
-				
-		(client_ip, client_port)=self.client_address
-		print(client_ip)
-		print(self.path)
-		parsed_data = urlparse.urlparse(self.path)
-		print(parsed_data.query)
-        	#get the query data and parse them into a dictionary
-		cdata_dict = urlparse.parse_qs(parsed_data.query)
-		androidId = cdata_dict['unique_id'][0]
-		cellularOperator = cdata_dict['network_operator'][0]
-		clientLocalIp = cdata_dict['local_ip'][0]
-		#prepare response dictionary to return as json
-		if clientLocalIp != client_ip:
-			NatProxy = True
-		if HttpProxy:
-			print("Http Proxy Found")
-			responseDict['HttpProxy'] ='yes'
-		else:
-			responseDict['HttpProxy'] = 'no'
-		responseDict['NatProxy'] = {}
-		if NatProxy:
-			print("Nat Proxy present")
-			responseDict['NatProxy']['NatPresent']='yes'
-			responseDict['NatProxy']['clientIp']=client_ip
-			responseDict['NatProxy']['clientPort'] = client_port
-			
-		else:
-			responseDict['NatProxy']['NatPresent']='no' 
-		print(responseDict)
-		json_string = json.dumps(responseDict)
-		self.send_response(200)
-		self.end_headers()
-		self.wfile.write(json_string)
+        for uagent in knownUserAgents:
+            if uagent not in reqHeaders['User-Agent']:
+                return True
+        return False
 
 
 from BaseHTTPServer import HTTPServer
